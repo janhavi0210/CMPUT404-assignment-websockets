863078d280ab42e850499b50e9dd6b54e676a7e7
janhavi0210 <75977053+janhavi0210@users.noreply.github.com>
2021-12-02 02:02:31 -0700

    Update sockets.py

--- sockets.py
+++ sockets.py
@@ -59,29 +59,86 @@ class World:
     def world(self):
         return self.space
 
-myWorld = World()        
+myWorld = World()
+
+#class Client, def send_all, and def send_all_json from https://github.com/abramhindle/WebSocketsExamples
+clients = list()
+
+
+
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+        
+
+
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+
+
+class Client:
+
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
-
+    for client in clients:
+        client.put(json.dumps({entity:data}))
+    
 myWorld.add_set_listener( set_listener )
+
+
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    #from https://github.com/janhavi0210/CMPUT404-assignment-ajax/blob/master/server.py
+    return redirect("/static/index.html", code=302)
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    #from https://github.com/abramhindle/WebSocketsExamples
+    try:
+        while True:
+            message = ws.receive()
+            
+            if (msg is not None):
+                data = json.loads(msg)
+                for entity in data:
+                    myWorld.set(entity, data[entity])
+            else:
+                break
+    except Exception as e:
+        print e
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )    
+    try:
+        while True:
+            
+            message = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +156,31 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    #from https://github.com/janhavi0210/CMPUT404-assignment-ajax/blob/master/server.py
+    for key,value in flask_post_json().items():
+        myWorld.update(entity,key,value)
+    return myWorld.get(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    #from https://github.com/janhavi0210/CMPUT404-assignment-ajax/blob/master/server.py
+    return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    #from https://github.com/janhavi0210/CMPUT404-assignment-ajax/blob/master/server.py
+    return myWorld.get(entity)
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    #from https://github.com/janhavi0210/CMPUT404-assignment-ajax/blob/master/server.py
+    myWorld.clear()
+    return myWorld.world()
+
 
 
 
